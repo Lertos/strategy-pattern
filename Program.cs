908caa901732cd75874b97c0f9e9a2b5f94cb44f
Lertos <deetasticgaming@gmail.com>
2024-05-26 12:51:34 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace strategy_pattern
+{
+    //A demonstration of the Strategy pattern in C#
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class SomeController
+    {
+        //Any type of file storage can be chosen as long as it implements the shared interface
+        IFileStorage fileStorage;
+
+        //Gives much more flexibility. You aren't concerned with "how" it is done, only that it is done.
+        public SomeController(IFileStorage fileStorage)
+        {
+            this.fileStorage = fileStorage;
+        }
+    }
+
+    interface IFileStorage
+    {
+        byte[] GetFile(string id);
+        void SaveFile();
+    }
+
+    class LocalStorage : IFileStorage
+    {
+        public byte[] GetFile(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class RemoteStorage : IFileStorage
+    {
+        public byte[] GetFile(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class VirtualStorage : IFileStorage
+    {
+        public byte[] GetFile(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
